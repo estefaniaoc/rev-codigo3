0a77f8f74ee82da3b7b4bf40f1470a09fe4071b0
estefaniaoc <ortizc.estefania@gmail.com>
2024-07-17 10:10:03 -0600

    Se modifican archivos

--- filtro.js
+++ filtro.js
@@ -1,63 +1,56 @@
-// Tenemos un li de productos
-
 const productos = [
   {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
   {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
   {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
   {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+];
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+// Cambiamos por el getElementById
+const listaDeProductos = document.getElementById("lista-de-productos");
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+// Cambiamos por getElementById y Seleccionamos el input
+const filtroInput = document.getElementById("filtro");
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+// Se agrega función para mostrar productos
+const displayProductos = (productos) => {
+  // Limpiamos la lista de productos
+  while (listaDeProductos.firstChild) {
+    listaDeProductos.removeChild(listaDeProductos.firstChild);
+  }
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div");
+    d.classList.add("producto");
 
-  li.appendChild(d)
-}
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productos[i].nombre;
 
-displayProductos(productos)
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
+
+    d.appendChild(ti);
+    d.appendChild(imagen);
+
+    listaDeProductos.appendChild(d);
+  }
+};
+
+//se utiliza la funcion para mostrar todos los productos al cargar la página
+displayProductos(productos);
+
+
 const botonDeFiltro = document.querySelector("button");
 
 botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
-  }
-
-  const texto = $i.value;
+  const texto = filtroInput.value.toLowerCase();
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+  const productosFiltrados = filtrado(productos, texto);
 
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
-    
-    var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
-  }
-}
+  displayProductos(productosFiltrados);
+};
 
 const filtrado = (productos = [], texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+};
